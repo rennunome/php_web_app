f789c6c7d0ad8a998a63e21c850864f37c6a9f8c
rennunome <r_nunome@emyus.jp>
2021-04-19 18:21:59 +0900

    Modified files for list function pt2

--- list.php
+++ list.php
@@ -7,30 +7,48 @@ require "header.php";
 <meta charset="UTF-8">
 <title>List</title>
 <body>
-<div align="center">
-<form action= "register.php" method= "post">
-<input type= "submit" value="新規登録" >
-</form>
-</div>
+	<div align="center">
+		<form action="register.php" method="post">
+			<input type="submit" value="新規登録" />
+		</form>
+	</div>
 <?php
 $sql = 'SELECT * FROM questions';
 $stmt = $db->prepare($sql);
 $stmt->execute();
-$qlist = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($q_list as $value) { ?>
-<label for="question_id">問題：<?php var_dump($value['id']);?></label>
-    <input type="text" name="question" id="question_id" value="<?php $value["question"]; ?>" />
+$q_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($q_list as $q_value) {
+    ?>
+<label for="question_id">問題：<?= $q_value['id'] ?></label>
+	<input type="text" name="question" id="question_id"
+		value="<?= $q_value["question"]; ?>" />
+	<br />
+<?php
+    $sql = 'SELECT * FROM correct_answers';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $ca_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($ca_list as $ca_value) {
+        if ($ca_value['id'] == $q_value['id']) {
+            ?>
+<label for="answer_id">答え：<?= $ca_value['id'] ?></label>
+	<input type="text" name="answer" id="answer_id"
+		value="<?= $ca_value["answer"]; ?>" />
+	<br />
+	<form action="edit.php" method="post">
+		<input type="hidden" name="question_id"
+			value="<?= $ca_value["id"]; ?>" /> <input type="submit" value="編集" />
+	</form>
+	<form action="delete.php" method="post">
+		<input type="hidden" name="question_id"
+			value="<?= $ca_value["id"]; ?>" /> <input type="submit" value="削除" />
+	</form>
 <?php
 }
 ?>
 <?php
-$sql = 'SELECT * FROM correct_answers';
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$ca_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($ca_list as $value) { ?>
-<label for="answer_id">答え：<?php var_dump($value['id']);?></label>
-    <input type="text" name="answer" id="answer_id" value="<?php $value["answer"]; ?>" />
+}
+?>
 <?php
 }
 ?>
